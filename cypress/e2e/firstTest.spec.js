1fbb17814ff911e09f515536aee8af8599d4ed01
Mamta Rani <er.mamta25@gmail.com>
2024-07-18 01:20:28 +0530

    As on 18th July'24, Completed Chapter-26- Until date picker

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,193 @@
+/// <reference types="cypress" />
+describe('Test Suite', () => {
+    it('Test case 1', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //by tag name
+        cy.get('input')
+
+        //by id
+        cy.get('#inputEmail1')
+
+        //by class value
+        cy.get('.input-full-width')
+
+        //by attribute name
+        cy.get('[fullwidth]')
+
+        //by attribute and value
+        cy.get('[placeholder="Email"]')
+
+        //by entire class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by two attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        //by tag, attribute, id and class
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        //by cypress test id
+        cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it('Text case 2', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //Thoery
+        //get() - find elements on the page by locator globally
+        //find() - find child elements by locator
+        //contains() - find HTML text and by text and locator
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in') // returns all elements from the page
+
+        //Cypress chains and DOM
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+    })
+
+    it('Save subject of the command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // WON'T WORK LIKE THIS 
+        // const usingTheGrid = cy.contains('nb-card', 'Using the Grid')
+        // usingTheGrid.find('[for="inputEmail1"]').should('contain', 'Email')
+        // usingTheGrid.find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // Approach 1- Cypress alias
+        cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+        cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // Approach 2- Cypress then() method
+        cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
+            cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
+            cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+
+        })
+    })
+
+    it('Extract text values', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2- JQuery method
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            const labelText = label.text()
+            expect(labelText).to.equal('Email address')
+            cy.wrap(labelText).should('contain', 'Email address')
+        })
+
+        //3- Cypress method- invoke
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        //4- Cypress method- invoke- without then
+        cy.get('[for="exampleInputEmail1"]').invoke('text').should('contain', 'Email address')
+
+        //5- Cypress method- invoke- without then- with alias as
+        cy.get('[for="exampleInputEmail1"]').invoke('text').as('labelText').should('contain', 'Email address')
+
+        //6- Cypress method- invoke attribute
+        cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then(classValue => {
+            expect(classValue).to.equal('label')
+        })
+
+        //7- Cypress method- invoke property
+        cy.get('#exampleInputEmail1').type('test@test.com')
+        cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com')
+
+        //8- Cypress method- invoke property using then
+        //cy.get('#exampleInputEmail1').type('test@test.com')
+        //cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com').then(property => {
+        // expect(property).to.equal('test@test.com')
+        //   })
+    })
+
+    it('Radio buttons', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+            cy.wrap(radioButtons).eq(0).check({ force: true }).should('be.checked')
+            cy.wrap(radioButtons).eq(1).check({ force: true })
+            cy.wrap(radioButtons).eq(0).should('not.be.checked')
+            cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+    })
+
+    it('Checkboxes', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+            cy.wrap(radioButtons).eq(0).check({ force: true }).should('be.checked')
+            cy.wrap(radioButtons).eq(1).check({ force: true })
+            cy.wrap(radioButtons).eq(0).should('not.be.checked')
+            cy.wrap(radioButtons).eq(2).should('be.disabled')
+        })
+    })
+
+    it('Checkboxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+        cy.get('[type="checkbox"]').check({ force: true })
+        cy.get('[type="checkbox"]').eq(0).click({ force: true })
+        cy.get('[type="checkbox"]').eq(1).check({ force: true })
+    })
+
+    it.only('Date picker', () => {
+
+        function selectDayFromCurrent(day) {
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleDateString('en-US', { month: 'short' })
+            let futureYear = date.getFullYear()
+            let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+            })
+            return dateToAssert
+        }
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+            cy.wrap(input).click()
+           const dateToAssert = selectDayFromCurrent(200)
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
+            cy.wrap(input).should('have.value', dateToAssert)
+        })
+
+    })
+
+})
